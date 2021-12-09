6f7318197c731a83cc674eba8e67074892898164
kti <tim.kaspar@ti8m.ch>
2021-12-09 11:28:01 +0100

    generate database.g.dart

---
+++ NightTime/lib/database/database.g.dart
@@ -0,0 +1,147 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'database.dart';
+
+// **************************************************************************
+// FloorGenerator
+// **************************************************************************
+
+// ignore: avoid_classes_with_only_static_members
+class $FloorFloorDB {
+  /// Creates a database builder for a persistent database.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$FloorDBBuilder databaseBuilder(String name) =>
+      _$FloorDBBuilder(name);
+
+  /// Creates a database builder for an in memory database.
+  /// Information stored in an in memory database disappears when the process is killed.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$FloorDBBuilder inMemoryDatabaseBuilder() => _$FloorDBBuilder(null);
+}
+
+class _$FloorDBBuilder {
+  _$FloorDBBuilder(this.name);
+
+  final String? name;
+
+  final List<Migration> _migrations = [];
+
+  Callback? _callback;
+
+  /// Adds migrations to the builder.
+  _$FloorDBBuilder addMigrations(List<Migration> migrations) {
+    _migrations.addAll(migrations);
+    return this;
+  }
+
+  /// Adds a database [Callback] to the builder.
+  _$FloorDBBuilder addCallback(Callback callback) {
+    _callback = callback;
+    return this;
+  }
+
+  /// Creates the database and initializes it.
+  Future<FloorDB> build() async {
+    final path = name != null
+        ? await sqfliteDatabaseFactory.getDatabasePath(name!)
+        : ':memory:';
+    final database = _$FloorDB();
+    database.database = await database.open(
+      path,
+      _migrations,
+      _callback,
+    );
+    return database;
+  }
+}
+
+class _$FloorDB extends FloorDB {
+  _$FloorDB([StreamController<String>? listener]) {
+    changeListener = listener ?? StreamController<String>.broadcast();
+  }
+
+  TimeDAO? _timeDAOInstance;
+
+  Future<sqflite.Database> open(String path, List<Migration> migrations,
+      [Callback? callback]) async {
+    final databaseOptions = sqflite.OpenDatabaseOptions(
+      version: 1,
+      onConfigure: (database) async {
+        await database.execute('PRAGMA foreign_keys = ON');
+        await callback?.onConfigure?.call(database);
+      },
+      onOpen: (database) async {
+        await callback?.onOpen?.call(database);
+      },
+      onUpgrade: (database, startVersion, endVersion) async {
+        await MigrationAdapter.runMigrations(
+            database, startVersion, endVersion, migrations);
+
+        await callback?.onUpgrade?.call(database, startVersion, endVersion);
+      },
+      onCreate: (database, version) async {
+        await database.execute(
+            'CREATE TABLE IF NOT EXISTS `time` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `toSleep` TEXT NOT NULL, `sleepTime` TEXT NOT NULL, `wakeUp` TEXT NOT NULL)');
+
+        await callback?.onCreate?.call(database, version);
+      },
+    );
+    return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+  }
+
+  @override
+  TimeDAO get timeDAO {
+    return _timeDAOInstance ??= _$TimeDAO(database, changeListener);
+  }
+}
+
+class _$TimeDAO extends TimeDAO {
+  _$TimeDAO(this.database, this.changeListener)
+      : _queryAdapter = QueryAdapter(database, changeListener),
+        _timeInsertionAdapter = InsertionAdapter(
+            database,
+            'time',
+            (Time item) => <String, Object?>{
+                  'id': item.id,
+                  'toSleep': item.toSleep,
+                  'sleepTime': item.sleepTime,
+                  'wakeUp': item.wakeUp
+                },
+            changeListener);
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final QueryAdapter _queryAdapter;
+
+  final InsertionAdapter<Time> _timeInsertionAdapter;
+
+  @override
+  Future<List<Time>> findAllTime() async {
+    return _queryAdapter.queryList('SELECT * FROM time',
+        mapper: (Map<String, Object?> row) => Time(
+            row['id'] as int,
+            row['toSleep'] as String,
+            row['sleepTime'] as String,
+            row['wakeUp'] as String));
+  }
+
+  @override
+  Stream<Time?> findTimeById(int id) {
+    return _queryAdapter.queryStream('SELECT * FROM time WHERE id = ?1',
+        mapper: (Map<String, Object?> row) => Time(
+            row['id'] as int,
+            row['toSleep'] as String,
+            row['sleepTime'] as String,
+            row['wakeUp'] as String),
+        arguments: [id],
+        queryableName: 'time',
+        isView: false);
+  }
+
+  @override
+  Future<void> insertTime(Time time) async {
+    await _timeInsertionAdapter.insert(time, OnConflictStrategy.abort);
+  }
+}
