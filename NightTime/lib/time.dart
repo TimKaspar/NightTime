00f922c80c880c7eb044df68afca08a875c91def
kti <tim.kaspar@ti8m.ch>
2021-12-15 09:08:33 +0100

    removed testdata add on initzialize

--- NightTime/lib/time.dart
+++ NightTime/lib/time.dart
@@ -1,4 +1,4 @@
 class Time{
-  static List<DateTime> toSleep = DateTime.daysPerWeek as List<DateTime>;
-  static List<DateTime> wakeUp = DateTime.daysPerWeek as List<DateTime>;
+  static List<DateTime> toSleep = [];
+  static List<DateTime> wakeUp = [];
 }
